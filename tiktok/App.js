feec483252db0215852175c1943a1bb7e9adcfb2
Adam Burrough <adamjamesburrough@gmail.com>
2023-09-19 16:31:41 +0900

    middleware added

--- tiktok/App.js
+++ tiktok/App.js
@@ -3,8 +3,14 @@ import { StyleSheet, Text, View } from "react-native";
 import Constants from "expo-constants";
 import { initializeApp } from "firebase/app";
 import firebaseSecret from "./secrets.json";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux-thunk";
+import thunk from "redux-thunk";
+import rootReducer from "./src/redux/reducers/index.js";
 
 const app = initializeApp(firebaseSecret);
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default function App() {
   return (
