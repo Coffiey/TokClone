54097e26d3dba27690f7968839885f81721a09f6
Adam Burrough <adamjamesburrough@gmail.com>
2023-11-13 19:19:26 +0900

    added message board format

---
+++ tiktok/src/components/chat/list/item/index.js
@@ -0,0 +1,29 @@
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import React from "react";
+import { useUser } from "../../../../hooks/useUser";
+import { auth } from "../../../../../App";
+import styles from "./styles";
+
+const ChatListItem = ({ chat }) => {
+  const { data: userData } = useUser(
+    chat.members[0] === auth.currentUser.uid ? chat.members[1] : chat.members[0]
+  );
+  console.log(data);
+  return (
+    <TouchableOpacity style={styles.container}>
+      <Image
+        style={styles.image}
+        source={{ uri: userData.photoUrl }}
+      />
+      <View style={{ flex: 1 }}>
+        <Text style={styles.userNameText}>{userData.displayName}</Text>
+        <Text style={styles.lastMessage}>{chat.lastMessage}</Text>
+      </View>
+      <Text style={styles.date}>
+        {chat.lastUpdate.toISOString().slice(0, 10)}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+export default ChatListItem;
--- tiktok/src/screens/chat/list/index.js
+++ tiktok/src/screens/chat/list/index.js
@@ -2,16 +2,23 @@ import { View, Text, FlatList } from "react-native";
 import React from "react";
 import NavBarGeneral from "../../../components/general/navBar";
 import { SafeAreaView } from "react-native-safe-area-context";
+import ChatListItem from "../../../components/chat/list/item";
 
 const ChatScreen = () => {
-  const renderItem = () => {
-    return <></>;
+  const renderItem = ({ item }) => {
+    return <ChatListItem chat={item} />;
   };
   return (
     <SafeAreaView>
       <NavBarGeneral leftButton={{ display: fals }} />
       <FlatList
-        data={[]}
+        data={[
+          {
+            lastUpdate: new Date(),
+            lastMessage: "test",
+            members: ["member 1", "member 2"],
+          },
+        ]}
         removeClippedSubviews
         renderItem={renderItem}
         keyExtractor={(item) => item.id}
