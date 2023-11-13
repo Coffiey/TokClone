f40dd6334b56a7d3963bb7ddd1e71172ad1561c4
Adam Burrough <adamjamesburrough@gmail.com>
2023-11-13 20:35:36 +0900

    delete: custom NavBar, due torendering falt

--- tiktok/src/redux/reducers/index.js
+++ tiktok/src/redux/reducers/index.js
@@ -2,7 +2,8 @@ import { combineReducers } from "redux";
 import { auth } from "./auth";
 import { posts } from "./posts";
 import { model } from "./model";
+import { chat } from "./chat";
 
-const Reducers = combineReducers({ auth, posts, model });
+const Reducers = combineReducers({ auth, posts, model, chat });
 
 export default Reducers;
